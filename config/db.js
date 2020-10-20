7b01b50cde3209e95edd931502c62f0a183f9150
xau327 <73146699+xau327@users.noreply.github.com>
2020-10-19 22:05:47 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,9 @@
 module.exports = {
 	url : 'mongodb://<user>:<user>@novus.modulusmongo.net:27017/V8ahipip'
 }
+/*const mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb://localhost", { useUnifiedTopology: true })
+            .then(conn => global.conn = conn.db("workshoptdc"))
+            .catch(err => console.log(err))
+
+module.exports = { }*/
