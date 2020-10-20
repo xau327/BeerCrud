2871ff7598269f9f66b7354ce7919fec1635627f
xau327 <73146699+xau327@users.noreply.github.com>
2020-10-19 22:24:34 -0300

    Update main.js
    adding more vars

--- public/main.js
+++ public/main.js
@@ -21,3 +21,18 @@ app.config(function($routeProvider){
 	$routeProvider.otherwise({redirectTo:'/'});
 
 });
+/*// view engine setup
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);*/
